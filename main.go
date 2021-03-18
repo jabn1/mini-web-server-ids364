f0c2830907c8a0f5b90f42d185e17e8fd03a0ea0
Jose <jose.berges23@gmail.com>
2021-03-17 22:21:30 -0400

    Finished

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 )
 
 var messages map[int]string
+var count int
 
 func main() {
 	port := "5000"
@@ -19,6 +20,7 @@ func main() {
 		1: "Hola",
 		2: "Mundo",
 	}
+	count = 3
 	r := registerRoutes()
 	fmt.Println("Listening on port :" + port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
@@ -41,29 +43,65 @@ func getMessages(w http.ResponseWriter, r *http.Request) {
 }
 
 func createMessage(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Invalid message", 400)
+		return
+	}
+	if len(body) == 0 {
+		http.Error(w, "Empty request body", 400)
+		return
+	}
 
+	messages[count] = string(body)
+	count += 1
 }
 
 func updateMessage(w http.ResponseWriter, r *http.Request) {
 	msgId := chi.URLParam(r, "msgId")
 	body, err := ioutil.ReadAll(r.Body)
 	if msgId == "" {
-		http.Error(w, "Invalid message id", 400)
+		http.Error(w, "Empty message id", 400)
 		return
 	}
 	if err != nil {
 		http.Error(w, "Invalid message", 400)
+		return
 	}
 	if len(body) == 0 {
 		http.Error(w, "Empty request body", 400)
+		return
 	}
 	id, err := strconv.Atoi(msgId)
 	if err != nil {
 		http.Error(w, "Invalid message id", 400)
+		return
 	}
-	messages[id] = string(body)
+
+	if _, exists := messages[id]; exists {
+		messages[id] = string(body)
+	} else {
+		http.Error(w, "Message id does not exist", 400)
+		return
+	}
 }
 
 func deleteMessage(w http.ResponseWriter, r *http.Request) {
+	msgId := chi.URLParam(r, "msgId")
+	if msgId == "" {
+		http.Error(w, "Empty message id", 400)
+		return
+	}
+	id, err := strconv.Atoi(msgId)
+	if err != nil {
+		http.Error(w, "Invalid message id", 400)
+		return
+	}
 
+	if _, exists := messages[id]; exists {
+		delete(messages, id)
+	} else {
+		http.Error(w, "Message id does not exist", 400)
+		return
+	}
 }
